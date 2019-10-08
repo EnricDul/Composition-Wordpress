b41858528b0d2661c34d8dd51c4f972dd42c392d
Maxime-Cao <maxime.cao20@gmail.com>
2019-10-08 12:28:20 +0200

    base fonction.php

--- functions.php
+++ functions.php
@@ -5,3 +5,10 @@ add_theme_support( 'post-thumbnails' );
 
 // Ajouter automatiquement le titre du site dans l'en-tête du site
 add_theme_support( 'title-tag' );
+
+function nom_scripts() {
+    wp_enqueue_style('boostrap',get_template_directory_uri().'/bootstrap.css',array(),'1.0.0','all');
+    wp_enqueue_style('theme_style',get_template_directory_uri().'/style.css',array("boostrap"),'1.0.0','all');
+    wp_enqueue_script('bootstrapScript',get_template_directory_uri().'/bootstrap.bundle.js',array('jquery'),'1.0.0',false);
+}
+    add_action('wp_enqueue_scripts','nom_scripts');
