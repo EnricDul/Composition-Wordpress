94563417fafe3e3f57fbb312ea6be61b700d2a02
Maxime-Cao <maxime.cao20@gmail.com>
2019-10-09 14:01:07 +0200

    sauvegarde au cas où

--- functions.php
+++ functions.php
@@ -7,8 +7,8 @@ add_theme_support( 'post-thumbnails' );
 add_theme_support( 'title-tag' );
 
 function nom_scripts() {
-    wp_enqueue_style('boostrap',get_template_directory_uri().'/bootstrap.css',array(),'1.0.0','all');
-    wp_enqueue_style('theme_style',get_template_directory_uri().'/style.css',array("boostrap"),'1.0.0','all');
-    wp_enqueue_script('bootstrapScript',get_template_directory_uri().'/bootstrap.bundle.js',array('jquery'),'1.0.0',false);
+    wp_enqueue_style('boostrap',get_template_directory_uri().'/assets/css/bootstrap.css',array(),'1.0.0','all');
+    wp_enqueue_style('theme_style',get_template_directory_uri().'/assets/css/style.css',array("boostrap"),'1.0.0','all');
+    wp_enqueue_script('bootstrapScript',get_template_directory_uri().'/assets/js/bootstrap.bundle.js',array('jquery'),'1.0.0',false);
 }
     add_action('wp_enqueue_scripts','nom_scripts');
--- header.php
+++ header.php
@@ -16,71 +16,141 @@
     >
     <header class="header">
       <nav class="navbar navbar-expand-lg navbar-light col-12 ">
-        <a class = "margin" href="<?php echo home_url( '/' ); ?>">
-            <img 
-              src="<?php echo get_template_directory_uri(); ?>/svg/logo-full.svg"
-              alt="Logo"
-            />
-          </a>
+        <a class="margin" href="<?php echo home_url( '/' ); ?>">
+          <img
+            src="<?php echo get_template_directory_uri(); ?>/assets/svg/logo-full.svg"
+            alt="Logo"
+          />
+        </a>
 
-          <a class="top-home" href="#"
-            > <!-- Logo en background en css  -->
-            <span class="sr-only">(current)</span></a
-          >
-       
-          <a class="nav-item nav-link active" href="#"
-            ><span class="black-underline">MAGASIN</span> 
-            <span class="sr-only">(current)</span></a
-          >
-          <a class="nav-item nav-link active" href="#"
-            ><span class="black-underline">ARCHITECTE D'INTERIEUR</span>
-            <span class="sr-only">(current)</span></a
-          >
-          <a class="nav-item nav-link active" href="#"
-            ><span class="black-underline">A PROPOS</span>
-            <span class="sr-only">(current)</span></a
-          >
-          <a class="nav-item nav-link active" href="#"
-            ><span class="black-underline">BLOG</span>
-            <span class="sr-only">(current)</span></a
-          >
-          <a class="nav-item nav-link active" href="#"
-            ><span class="black-underline">CONTACT</span>
-            <span class="sr-only">(current)</span></a
-          >
-          
-          <!-- Menu hamburger -->
-          <button type="button" class="btn btn-hamb btn-wish" data-toggle="dropdown" id="dropdownMenuButton">
-            
-            <span id="wishlist">Wishlist</span><img class="burger"
-              src="<?php echo get_template_directory_uri(); ?>/svg/bullet-list.svg"
-              alt="burger"
-            />
-          </button>
-                    
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            
+        <a class="top-home nav-items-desktop" href="#">
+          <!-- Logo en background en css  -->
+          <span class="sr-only">(current)</span></a
+        >
+
+        <a class="nav-item nav-link active nav-items-desktop " href="#"
+          ><span class="black-underline">MAGASIN</span>
+          <span class="sr-only">(current)</span></a
+        >
+        <a class="nav-item nav-link active nav-items-desktop " href="#"
+          ><span class="black-underline">ARCHITECTE D'INTERIEUR</span>
+          <span class="sr-only">(current)</span></a
+        >
+        <a class="nav-item nav-link active nav-items-desktop " href="#"
+          ><span class="black-underline">A PROPOS</span>
+          <span class="sr-only">(current)</span></a
+        >
+        <a class="nav-item nav-link active nav-items-desktop " href="#"
+          ><span class="black-underline">BLOG</span>
+          <span class="sr-only">(current)</span></a
+        >
+        <a class="nav-item nav-link active nav-items-desktop " href="#"
+          ><span class="black-underline">CONTACT</span>
+          <span class="sr-only">(current)</span></a
+        >
+      
+
+        <!-- Menu hamburger -->
+        <button
+          type="button"
+          class="btn btn-hamb btn-wish"
+          data-toggle="dropdown"
+          id="dropdownMenuButton"
+        >
+          <span id="wishlist">Wishlist</span
+          ><img
+            class="burger"
+            src="<?php echo get_template_directory_uri(); ?>/assets/svg/bullet-list.svg"
+            alt="burger"
+          />
+        </button>
+
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
           <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-           
-                </div>
-<!-- FIN NAV BAR STANDAR -->
-<!-- ON VEUT LA METTRE ICI -->
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email address</label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted"
+                >We'll never share your email with anyone else.</small
+              >
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <div class="form-group form-check">
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="exampleCheck1"
+              />
+              <label class="form-check-label" for="exampleCheck1"
+                >Check me out</label
+              >
+            </div>
 
+            <button type="submit" class="btn btn-dark">Submit</button>
+          </form>
+        </div>
+
+        <!--Menu hamburger pour responsive tablette et mobile -->
+
+        <div class="pos-f-t nav-items-tablet">
+        <nav class="navbar navbar-dark bg-dark burger-X">
+            <button
+              class="btn btn-hamb menu-btn "
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            > MENU
+              <span class="navbar-toggler-icon tae"></span>
+            </button>
+          </nav>
+          <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg">
+              <ul class="menu-site-hamburger">
+                <li> <a class="nav-item item-tabl nav-link active" href="#"
+          ><span class="black-underline">MAGASIN</span>
+          <span class="sr-only">(current)</span></a
+        ></li>
+                <li><a class="nav-item item-tabl nav-link active" href="#"
+          ><span class="black-underline">ARCHITECTE D'INTERIEUR</span>
+          <span class="sr-only">(current)</span></a
+        ></li>
+                <li> <a class="nav-item item-tabl nav-link active" href="#"
+          ><span class="black-underline">A PROPOS</span>
+          <span class="sr-only">(current)</span></a
+        ></li>
+                <li><a class="nav-item item-tabl nav-link active" href="#"
+          ><span class="black-underline">BLOG</span>
+          <span class="sr-only">(current)</span></a
+        ></li>
+                <li> <a class="nav-item item-tabl nav-link active" href="#"
+          ><span class="black-underline">CONTACT</span>
+          <span class="sr-only">(current)</span></a
+        ></li>
+
+              </ul>
+            </div>
+          </div>
+          
+        </div>
+        <!-- FIN NAV BAR STANDAR -->
+        <!-- ON VEUT LA METTRE ICI -->
       </nav>
     </header>
 
